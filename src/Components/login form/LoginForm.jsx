f050ea7b8ba26dbce49c98895fc3b6bcb8c389be
Harshita Maheshwari <hmaheshwari2003@gmail.com>
2024-08-24 01:15:19 +0530

    commit 2

---
+++ src/Components/login form/LoginForm.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import { FaGoogle,FaFacebook } from "react-icons/fa";
+
+function LoginForm () {
+
+  
+    return (
+        <section>
+          <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
+            <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+              
+              <h1 className="text-center text-2xl font-bold leading-tight text-black">
+                Sign up to create account
+              </h1>
+              
+              <p className="mt-2 text-center text-base text-gray-600">
+                Already have an account?
+                <a
+                  href="#"
+                  title=""
+                  className="font-medium text-black transition-all duration-200 hover:underline"
+                >
+                  Sign In &#8680;
+                </a>
+              </p>
+              <hr/>
+              <br/>
+              <form action="#" method="POST" className="mt-8">
+                <div className="space-y-5">
+                  <div>
+                    <label htmlFor="name" className="text-base font-medium text-gray-900">
+                      
+                      Full Name
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                        type="text"
+                        placeholder="Full Name"
+                        id="name"
+                      ></input>
+                    </div>
+                  </div>
+                  <div>
+                    <label htmlFor="email" className="text-base font-medium text-gray-900">
+                      
+                      Email address
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                        type="email"
+                        placeholder="Email"
+                        id="email"
+                      ></input>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label htmlFor="password" className="text-base font-medium text-gray-900">
+                        
+                        Password
+                      </label>
+                    </div>
+                    <div className="mt-2">
+                      <input
+                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                        type="password"
+                        placeholder="Password"
+                        id="password"
+                      ></input>
+                    </div>
+                  </div>
+                  <br/>
+                  <div>
+                
+                    <button
+                      type="button"
+                      className="inline-flex w-full items-center justify-center rounded-md bg-black px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-black/80"
+                    >
+                      Create Account 
+                    </button>
+                  </div>
+                </div>
+                <br/>
+              </form>
+
+              <div className="flex flex-col mt-3 space-y-3">
+
+                <div> 
+                <button
+                  type="button"
+                  className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
+                >
+                <div>Sign up with Google<center><FaGoogle color='red'/></center></div>
+                </button>
+                </div>
+               
+                <br/>
+                <button
+                  type="button"
+                  className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
+                >
+                <div>Sign up with Facebook <center><FaFacebook color='blue'/></center>
+                </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        </section>
+      )
+}
+ 
+
+export default LoginForm;
